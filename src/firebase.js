4c8a6d62ab9059b550460615e2205a94b57bb4d3
Ahmed Radi <46751565+idrissiradi@users.noreply.github.com>
2021-02-21 20:41:36 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCle9YHnYYx-1TJrSpROsYyaoRrBE02tE8",
-	authDomain: "drive-clone-react.firebaseapp.com",
-	projectId: "drive-clone-react",
-	storageBucket: "drive-clone-react.appspot.com",
-	messagingSenderId: "284632317660",
-	appId: "1:284632317660:web:dcda9721fe8d77917a3e19",
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
